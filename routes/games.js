00ecc5eb92a418f55047dc37716c60d431724710
Jacky <jackylai1030@gmail.com>
2026-06-20 12:53:33 -0400

    added /games/:id route with error handling

--- routes/games.js
+++ routes/games.js
@@ -6,4 +6,13 @@ router.get('/', (req,res) => {
     res.json(games);
 })
 
+router.get('/:id', (req,res) => {
+    const gameId = parseInt(req.params.id)
+    const game = games.find((g) => g.id === gameId);
+    if(!game){
+        return res.status(404).json({Message: 'Game not found'});
+    }
+    res.json(game);
+});
+
 export default router;
