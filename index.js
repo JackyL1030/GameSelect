0deb2142878e638672e46b0ae63de0498e30bb41
Jacky <jackylai1030@gmail.com>
2026-06-21 10:55:02 -0400

    added middleware

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import gamesRouter from './routes/games.js'
+import gamesRouter from "./routes/games.js";
 import games from "./data/games.js";
 
-import reviewsRouter from './routes/reviews.js'
-import usersRouter from './routes/users.js'
+import reviewsRouter from "./routes/reviews.js";
+import usersRouter from "./routes/users.js";
 
 const app = express();
 const PORT = 3000;
@@ -11,16 +11,28 @@ const PORT = 3000;
 app.use(express.json());
 
 app.set("view engine", "ejs");
-app.set("views", "./views")
+app.set("views", "./views");
 
+function logger(req, res, next) {
+  console.log(req.method + " " + req.url);
+  next();
+}
+
+function timeStamp(req, res, next) {
+  req.time = new Date().toLocaleString();
+  next();
+}
+
+app.use(logger);
+app.use(timeStamp);
 
 app.get("/", (req, res) => {
   res.send("Video Game Database Server Running");
 });
 
-app.use('/games', gamesRouter);
-app.use('/reviews', reviewsRouter);
-app.use('/users',usersRouter);
+app.use("/games", gamesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
